156f05b692016363a4efdca1137d9a9d45f354b9
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2024-07-22 18:12:59 -0400

    Version 0.9

---
+++ main.js
@@ -0,0 +1,45 @@
+const projects = [
+    {
+        title: "Anthocito (Bot de Discord)",
+        image: "/imagenes/discord-bot.jpg",
+        description: "Bot de Discord con diferentes funciones, de entretenimiento, moderación de servidores entre otros.",
+        github: "https://github.com/AnthoFu/Anthocito"
+    },
+    {
+        title: "Portafolio personal",
+        image: "/imagenes/Anthofu Logo - Blanco.png",
+        description: "Portafolio web, todo el codigo que tiene esta pagina.",
+        github: "https://github.com/AnthoFu/Portfolio"
+    },
+    // Agrega más proyectos aquí
+];
+
+let currentProject = 0;
+
+function displayProject(index) {
+    const project = projects[index];
+    document.getElementById("project-title").innerText = project.title;
+    document.getElementById("project-image").src = project.image;
+    document.getElementById("project-description").innerText = project.description;
+
+    const githubButton = document.getElementById("github-button");
+    if (project.github) {
+        githubButton.style.display = "inline-block";
+        githubButton.firstElementChild.href = project.github;
+    } else {
+        githubButton.style.display = "none";
+    }
+}
+
+function nextProject() {
+    currentProject = (currentProject + 1) % projects.length;
+    displayProject(currentProject);
+}
+
+function previousProject() {
+    currentProject = (currentProject - 1 + projects.length) % projects.length;
+    displayProject(currentProject);
+}
+
+// Inicializa el primer proyecto
+displayProject(currentProject);
